88700e3d863930e5021522c2008ea2c801db6c02
samane ghodrati <samane.ghodrati@gmail.com>
2024-12-15 22:38:47 -0900

    add

--- 1-countItems.js
+++ 1-countItems.js
@@ -1,1 +1,3 @@
 //Count the number of <li> in the countItems.html file and show the results in console.
+const countLi =document.querySelectorAll('li');
+console.log(countLi.length);
--- 2-createDom.js
+++ 2-createDom.js
@@ -8,3 +8,23 @@
 //todo8. Change style of each paragraph using JavaScript(eg. color, background, border, font-size, font-family)
 //todo9. Select all paragraphs and loop through each elements and give the first and third paragraph a color of green, and the second and the fourth paragraph a red color
 
+const para =document.querySelector('p');
+const paraEl=document.querySelector('#para');
+const paraEle =document.querySelectorAll('p');
+paraEle.forEach(p=>console.log(p.innerText));
+paraEle[3].textContent="lorem impson";
+paraEle.forEach(par=>par.classList.add('para'));
+paraEle.forEach(par=>{
+    par.style.color="red";
+    par.style.background="white";
+    par.style.border="1px solid";
+    par.style.fontSize="2 px";
+});
+for (let i = 0; i < paraEle.length ; i++) {
+   if(i%2===0){
+    paraEle[i].style.color="green"
+   }else{
+    paraEle[i].style.color="red"
+   }
+    
+}
