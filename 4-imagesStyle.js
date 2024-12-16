4ddbfc990c32bd110a7728ed9f00c81966ddee91
samane ghodrati <samane.ghodrati@gmail.com>
2024-12-16 00:26:58 -0900

    add

--- 4-imagesStyle.js
+++ 4-imagesStyle.js
@@ -1,3 +1,11 @@
 //todo1: Give all the images at even position a similar border color and style
 //todo2: and all the images at odd position same border style but different from the even ones.
 //*hint: iterate over the images using loops
+const img =document.querySelectorAll("img");
+img.forEach((image, index) => {
+    if (index % 2 === 0) {
+      image.style.border = '2px solid red'; // Even index, red border
+    } else {
+      image.style.border = '2px solid blue'; // Odd index, blue border
+    }
+  });
--- 5-lettersColor.js
+++ 5-lettersColor.js
@@ -5,3 +5,14 @@ todo2: each letter of the given text should pick a random color from the colors 
 todo3: finally display the text in the html file.
 */
 const colors = ['red', 'aqua', 'orange', 'purple', 'lightgreen'];
+
+
+const text = prompt("Enter your text:");
+let coloredText = "";
+
+for (let i = 0; i < text.length; i++) {
+  const randomColor = colors[Math.floor(Math.random() * colors.length)];
+  coloredText += `<span style="color: ${randomColor}">${text[i]}</span>`;
+}
+
+document.body.innerHTML = coloredText;
